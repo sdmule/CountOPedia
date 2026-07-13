2364c19c9275204386487867164a8132921a3cde
sdmule <mulesaurabh45@gmail.com>
2026-07-13 18:23:26 +0530

    Header and Footer Components

--- countopedia/src/main.jsx
+++ countopedia/src/main.jsx
@@ -1,8 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Header from "./Layout/header";
+import Footer from "./Layout/footer";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h1>Hello World!</h1>
+    <div className="d-flex flex-column min-vh-100">
+      <Header />
+      <main className="flex-fill"></main>
+      <Footer />
+    </div>
   </StrictMode>,
 );
