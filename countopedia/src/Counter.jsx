175fa1975739f2d4d0d13d065ed772471a4b412f
sdmule <mulesaurabh45@gmail.com>
2026-07-13 18:54:05 +0530

    Count is not updating in UI

--- countopedia/src/Counter.jsx
+++ countopedia/src/Counter.jsx
@@ -1,15 +1,23 @@
 function Counter() {
   let count = 0;
+
+  function handleIncrement() {
+    count += 1;
+    console.log(count);
+  }
+  function handleDecrement() {
+    count -= 1;
+  }
+
   return (
     <div className="row text-white container">
       Counter:{count}
-      <button
-        onClick={() => alert("Button was clicked")}
-        className="btn btn-success m-2"
-      >
+      <button onClick={handleIncrement} className="btn btn-success m-2">
         +1
       </button>
-      <button className="btn btn-danger m-2">-1</button>
+      <button onClick={handleDecrement} className="btn btn-danger m-2">
+        -1
+      </button>
     </div>
   );
 }
