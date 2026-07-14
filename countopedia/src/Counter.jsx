5f6c93cb2a4ebee85f1a70fee6050241986434fb
sdmule <mulesaurabh45@gmail.com>
2026-07-14 10:49:53 +0530

    State, useState() and UI updates to make a Game

--- countopedia/src/Counter.jsx
+++ countopedia/src/Counter.jsx
@@ -1,23 +1,60 @@
+import { useState } from "react";
+import attackImage from "./images/attack.png";
+import defenceImage from "./images/defend.png";
+
 function Counter() {
-  let count = 0;
+  let [count, setCount] = useState(0);
 
   function handleIncrement() {
-    count += 1;
-    console.log(count);
+    setCount(function (prev) {
+      return prev + 1;
+    });
   }
   function handleDecrement() {
-    count -= 1;
+    setCount(function (prev) {
+      return prev - 1;
+    });
+    // OR
+    //setCount((prev) => prev - 1);
   }
 
   return (
-    <div className="row text-white container">
-      Counter:{count}
-      <button onClick={handleIncrement} className="btn btn-success m-2">
-        +1
-      </button>
-      <button onClick={handleDecrement} className="btn btn-danger m-2">
-        -1
-      </button>
+    <div className="container">
+      <div className="row text-white container">
+        <h1>Game Score:{count}</h1>
+        <p>You win at +5 points and lose at -5 points</p>
+        {/* <p>Last Play: </p> */}
+        <h3>Game Status : </h3>
+
+        <div className="col-6 col-md-3 offset-md-3">
+          <img
+            src={attackImage}
+            style={{
+              width: "100%",
+              cursor: "pointer",
+              border: "1px solid green",
+            }}
+            className="p-4 rounded"
+          ></img>
+        </div>
+        <div className="col-6 col-md-3 offset-md-3">
+          <img
+            src={defenceImage}
+            style={{
+              width: "100%",
+              cursor: "pointer",
+              border: "1px solid red",
+            }}
+            className="p-4 rounded"
+          ></img>
+        </div>
+        <button onClick={handleIncrement} className="btn btn-success m-2">
+          +1
+        </button>
+        <button onClick={handleDecrement} className="btn btn-danger m-2">
+          -1
+        </button>
+      </div>
     </div>
   );
 }
